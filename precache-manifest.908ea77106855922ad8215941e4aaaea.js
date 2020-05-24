32f484074c310acddfba4438d116ca51d697e139
kanishkam <kanishka.zincat@gmail.com>
2020-05-24 13:42:06 +0530

    Updates

---
+++ precache-manifest.908ea77106855922ad8215941e4aaaea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223ca151513431d0f7ae0e7b2f3f6c6d",
+    "url": "/inventory-ui/index.html"
+  },
+  {
+    "revision": "6f171d261b5d8b5f24ba",
+    "url": "/inventory-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b6fc7c5442ab5f2d8da",
+    "url": "/inventory-ui/static/js/2.2ab49031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inventory-ui/static/js/2.2ab49031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f171d261b5d8b5f24ba",
+    "url": "/inventory-ui/static/js/main.b4b97145.chunk.js"
+  },
+  {
+    "revision": "71afa3db4e2e2faa8342",
+    "url": "/inventory-ui/static/js/runtime-main.41b7ecf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inventory-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
